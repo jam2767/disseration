09057d03d2dc8bba356cdcfb77cb282bd2d6348b
Joshua Mantooth <josh@36-53-41-155-wireless1x.bu.edu>
2016-04-21 09:36:18 -0400

    code to identify common species for treering analysis. Purpose is to limit analysis to species where I actually have enough to crossdate.

---
+++ treering_common_spp.R
@@ -0,0 +1,29 @@
+#identify common species for treering analysis
+#identify species with most individuals and how many sites
+#they are found in. Looking for enough individuals to xdate
+#and ability to look across scales - region, site, plot, sub,
+#individual
+
+#read adult data
+adult <- read.csv("/Users/Josh/Dropbox/Dietze_Lab_Undergrads/JAM - Xsite/Field Data Entry/Data Sheets/Adult_Field_Data_JAM_MCD.csv")
+
+#subset for trees > 15cm DBH
+big <- which(adult$DBH15 >= 15)
+adult.big <- adult[big,]
+
+#subset for stem 1 #may miss a couple of trees that were cored but were stem 2
+#just check those by hand
+leader <- which(adult.big$Num == 1)
+adult.cored <- adult.big[leader,]
+
+#number of cores per site
+site.num <- count(adult.cored, vars = "Site")
+
+#identify spp numbers
+spp.num <- count(adult.cored, vars = "Spp")
+
+#identify species by site
+spp.site.num <- count(adult.cored, vars = c("Spp","Site"))
+print(spp.site.num)
+write.csv(spp.site.num, file = "/Users/josh/Dropbox/Dissertation/CH1_Treerings/Data/species_by_site.csv")
+
