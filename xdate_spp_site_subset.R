d18d938d1655ef4139de0678065c960b642c8851
Joshua Mantooth <josh@36-53-41-155-wireless1x.bu.edu>
2016-04-22 11:30:04 -0400

    script to create folders for each species at each site for xdating. Identify outstanding cores.

---
+++ xdate_spp_site_subset.R
@@ -0,0 +1,6 @@
+#subsetting data by species and reordering folders for xdating
+#crossdate species at each site individually using cofecha
+#create species folders within sites
+#identify missing trees from census data and tucson file matches
+
+#match tucson files and adult data for each site
